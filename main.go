19860d06bc3454441dd1369be0ae3d39c22bf027
crazyfacka <gmail@joagonca.com>
2021-04-25 10:22:20 +0100

    Added TODO notes and closed initial implementation

--- main.go
+++ main.go
@@ -7,6 +7,11 @@ import (
 )
 
 func main() {
+	// TODO Validate if user already has a struct on loggedUsers and reuse token
+	// TODO Pick current workweek, from Mon to Fri
+
+	// TODO LOW Store user token in DB to persist across restarts
+
 	viper.SetConfigName("config")
 	viper.AddConfigPath(".")
 	if err := viper.ReadInConfig(); err != nil {
